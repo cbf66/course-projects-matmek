f27d612cb62758fab84d8000f1681b21154e2bbe
Charles Frankston <cbf@mit.edu>
2026-09-01 22:13:51 -0400

    differentiate
    du1 is a for loop
    du2 uses Python vectors

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,9 +2,17 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
+    r = np.zeros(len(u)-1)
+    for i in range(0, len(u)-1):
+        r[i] = (u[i + 1] - u[i]) / dt
+    return r
     raise NotImplementedError
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
+    return [x for x in np.diff(u) / dt]
+    # np.gradient(u, dt) would also work, but it behaves differently at the boundaries.
+    # For example, if u = [0, 1, 2], then np.gradient(u, dt) returns [1/dt, 1/dt, 1/dt], while the above implementation returns [1/dt, 1/dt].
+    # return np.gradient(u, dt)
     raise NotImplementedError
 
 def test_differentiate():
