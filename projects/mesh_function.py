acb2902fe14262130ebb860df4138ff4f01d6bdf
Charles Frankston <cbf@mit.edu>
2026-09-02 18:33:55 -0400

    Implement mesh_function to more closely follow the spec

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -5,13 +5,9 @@ import numpy as np
 
 def mesh_function(f: Callable[[float], float], t: np.ndarray) -> np.ndarray:
     return f(t)
-    raise NotImplementedError
-
 
 def func(t: float) -> float:
-    return [np.exp(-x) if (x < 4.0) else np.exp(-3*x) for x in t]
-    raise NotImplementedError
-
+    return [np.exp(-x) if (x <= 3.0) and (x >= 0) else (np.exp(-3*x) if (x <= 4.0) else 0.0) for x in t]
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
