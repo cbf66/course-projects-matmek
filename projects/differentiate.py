1eaa7e3ab9b3b6528e6420de4159f2012af1b844
Charles Frankston <cbf@mit.edu>
2026-09-02 18:14:35 -0400

    Differentiate according to Crank–Nicolson
    Also add a third method that just uses numpy.gradient :-)

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,26 +2,33 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    r = np.zeros(len(u)-1)
-    for i in range(0, len(u)-1):
-        r[i] = (u[i + 1] - u[i]) / dt
-    return r
-    raise NotImplementedError
-
+    # Crank–Nicolson
+    d = np.zeros_like(u)
+    d[0] = (u[1] - u[0]) / dt
+    for i in range(1, len(u)-1):
+        d[i] = (u[i + 1] - u[i - 1]) / (2 * dt)
+    d[-1] = (u[-1] - u[-2]) / dt
+    return d
+  
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    return [x for x in np.diff(u) / dt]
-    # np.gradient(u, dt) would also work, but it behaves differently at the boundaries.
-    # For example, if u = [0, 1, 2], then np.gradient(u, dt) returns [1/dt, 1/dt, 1/dt], while the above implementation returns [1/dt, 1/dt].
-    # return np.gradient(u, dt)
-    raise NotImplementedError
+    d = np.zeros_like(u)
+    d[0] = (u[1] - u[0]) / dt
+    d[1:-1] = (u[2:] - u[0:-2])/(2*dt)
+    d[-1] = (u[-1] - u[-2]) / dt
+    return d
 
+def differentiate_numpy(u: np.ndarray, dt: float) -> np.ndarray:
+     return np.gradient(u, dt)
+  
 def test_differentiate():
     t = np.linspace(0, 1, 10)
     dt = t[1] - t[0]
     u = t**2
     du1 = differentiate(u, dt)
     du2 = differentiate_vector(u, dt)
+    du3 = differentiate_numpy(u, dt)
     assert np.allclose(du1, du2)
+    assert np.allclose(du2, du3)
 
 if __name__ == '__main__':
     test_differentiate()
